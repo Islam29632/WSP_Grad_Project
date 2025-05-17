343c62c1eccab856d87bedd0d29a61c63beb9f11
Islam29632 <147077816+Islam29632@users.noreply.github.com>
2025-05-17 19:17:00 +0300

    mlp.py

--- backend/models/mlp.py
+++ backend/models/mlp.py
@@ -2,10 +2,9 @@ from keras.models import Sequential
 from keras.layers import Input, Dense
 
 
-def build_mlp_model(trial, input_shape):
+def build_mlp_model(mlp_best, input_shape):
     model = Sequential()
     model.add(Input(shape=(input_shape[1],)))  
-    model.add(Dense(units=trial.suggest_int("units", 32, 128), activation="relu"))
+    model.add(Dense(units=mlp_best['units'], activation="relu"))
     model.add(Dense(1))
-    model.compile(optimizer="adam", loss="mse")
     return model
