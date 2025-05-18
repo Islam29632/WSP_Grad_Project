ac037e230dc33c78b0e4e30d2db5b1c6bd16403d
EsraaKamel11 <Israa.kamel811@gmail.com>
2025-05-18 14:55:31 +0300

    Update mlp.py

--- backend/models/mlp.py
+++ backend/models/mlp.py
@@ -2,9 +2,14 @@ from keras.models import Sequential
 from keras.layers import Input, Dense
 
 
-def build_mlp_model(mlp_best, input_shape):
+def build_mlp_model(trial, input_shape, params=None):
     model = Sequential()
-    model.add(Input(shape=(input_shape[1],)))  
-    model.add(Dense(units=mlp_best['units'], activation="relu"))
-    model.add(Dense(1))
+    model.add(Input(shape=(input_shape[1],)))  # Ensure input shape is correct
+
+    # Get units from params or Optuna trial
+    units = params["units"] if params else trial.suggest_int("units", 32, 128)
+
+    model.add(Dense(units=units, activation="relu"))
+    model.add(Dense(1))  # Output layer
+
     return model
